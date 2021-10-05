6757ddc2626cb4fc5bd09e7df402130c040c34fa
shaunvan119 <shaunvan@live.com.au>
2021-10-05 16:03:37 +1100

    js input colors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function generateHex(){
 }
 
 function randomColors() {
-    colorDivs.forEach((div,index) =>{
+    colorDivs.forEach((div, index) => {
         const hexText = div.children[0];
         const randomColor = generateHex();
 
@@ -27,7 +27,16 @@ function randomColors() {
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
     //check for contrast
-    checkTextContrast(randomColor,hexText)
+    checkTextContrast(randomColor,hexText);
+    //Initail colorize sliders
+
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
     });
 }
 
@@ -40,6 +49,28 @@ function checkTextContrast(color, text) {
     }
 }
 
+function colorizeSliders(color, hue, brightness, saturation) {
+    //scale saturation
+    const noSat = color.set("hsl.s", 0);
+    const fullSat = color.set("hsl.s", 1);
+    const scaleSat = chroma.scale([noSat,color,fullSat]);
+    //Scale Brightness
+
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+
+    //update input colors
+
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, 
+    ${scaleSat(1)})`;
+
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)}, 
+    ${scaleBright(0.5)}, ${scaleBright(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75))`;
+}
+
 randomColors();
+
 
 
